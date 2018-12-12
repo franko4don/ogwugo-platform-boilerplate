20b0bba1e02942526a076a45a8cfdf55f5ac02c3
franko4don <franko4don@yahoo.com>
2018-12-12 17:22:30 +0100

    added app, organization, feature and subscription controller

---
+++ app/Api/V1/Controllers/AppController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Helpers\Helper;
+
+class Appcontroller extends Controller
+{
+    
+}
